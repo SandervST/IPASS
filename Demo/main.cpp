d9f600f2fde3fb8ea6a5b985e4d73d4b8355af84
Sander van Sterkenburg <31764843+SandervST@users.noreply.github.com>
2018-06-28 21:13:50 +0200

    Demo

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -1,3 +1,13 @@
+// ==============================================================
+//
+// Copyright Sander van Sterkenburg 2018
+//      
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+// ==============================================================
+
 #include "hwlib.hpp"
 #include "MPU6050.hpp"
 
